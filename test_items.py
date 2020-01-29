39a1967b63fc2545a8908d643682883e60b88963
Ilya Samorodov <ilyasamorodov@gmail.com>
2020-01-30 01:56:32 +0300

    Debug lines added to test method. Fix button var name

--- test_items.py
+++ test_items.py
@@ -1,6 +1,10 @@
+# import time
+
+
 def test_product_page_has_add_to_basket_button(browser):
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
-    add_to_basket_button = browser.find_elements_by_css_selector(".btn-add-to-basket")
+    # time.sleep(10)
+    basket_button = browser.find_elements_by_css_selector(".btn-add-to-basket")
     # I use find_elements_ instead of find_element because it returns an array
     # and helps to avoid NoSuchElementException in this case
-    assert len(add_to_basket_button) > 0, "Add to basket button not found"
+    assert len(basket_button) > 0, "Add to basket button not found"
